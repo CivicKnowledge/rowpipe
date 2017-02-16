83e3e2fe6eeb22a00b145a49babfd7005049ad6a
Eric Busboom <eric@clarinova.com>
2017-02-15 21:56:12 -0800

    more hacking

--- rowpipe/valuetype/geo.py
+++ rowpipe/valuetype/geo.py
@@ -5,7 +5,7 @@ Copyright (c) 2015 Civic Knowledge. This file is licensed under the terms of
 the Revised BSD License, included in this distribution as LICENSE.txt
 
 """
-import geoid.geoid.core
+
 from .core import *
 
 import geoid
